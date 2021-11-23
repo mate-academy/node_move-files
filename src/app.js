619e13f5e9e472dbe7245397fafca9ec305cbfae
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-11-23 23:29:27 +0200

    Move files solution added

--- src/app.js
+++ src/app.js
@@ -1,21 +1,45 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+import readline from 'readline';
+import fs from 'fs';
 
-module.exports = sum;
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+terminal.question("Input the command in format: 'mv file.txt ./someDirectory/'\n", (input) => {
+  const words = input.split(' ');
+
+  if (words.length === 3 && words[0] === 'mv') {
+    if (words[2].charAt(words[2].length - 1) !== '/') {
+      throw new Error("The name of destination directory should be ended with '/'");
+    };
+
+    fs.readFile(words[1], (err, content) => {
+      if (err) {
+        throw err;
+        terminal.close();
+      }
+
+      fs.writeFile(words[2] + "/" + words[1], content, (err, data) => {
+        if (err) {
+          throw err;
+          terminal.close();
+        }
+
+        fs.unlink(words[1], (err) => {
+          if (err) {
+            throw err;
+            terminal.close();
+          }
+            console.log("Success.");
+            terminal.close();
+        });
+      });
+    });
+  } else {
+    console.log("Please, check the input data");
+    terminal.close();
+  }
+})
