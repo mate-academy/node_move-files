04fc9f74a166a7c2e87a0dc2e32f4445f81cd133
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-12-01 13:49:05 +0200

    Fixes after review

--- src/app.js
+++ src/app.js
@@ -3,43 +3,28 @@
 import readline from 'readline';
 import fs from 'fs';
 
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const args = process.argv.slice(3);
 
-terminal.question("Input the command in format: 'mv file.txt ./someDirectory/'\n", (input) => {
-  const words = input.split(' ');
+if (args[1][args[1].length - 1] !== '/') {
+  console.log(args[1]);
+  throw new Error("The name of destination directory should be ended with '/'");
+}
 
-  if (words.length === 3 && words[0] === 'mv') {
-    if (words[2].charAt(words[2].length - 1) !== '/') {
-      throw new Error("The name of destination directory should be ended with '/'");
-    };
+fs.readFile(args[0], (err, content) => {
+  if (err) {
+    throw err;
+  }
 
-    fs.readFile(words[1], (err, content) => {
+  fs.writeFile(args[1] + "/" + args[0], content, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    fs.unlink(args[0], (err) => {
       if (err) {
         throw err;
-        terminal.close();
       }
-
-      fs.writeFile(words[2] + "/" + words[1], content, (err, data) => {
-        if (err) {
-          throw err;
-          terminal.close();
-        }
-
-        fs.unlink(words[1], (err) => {
-          if (err) {
-            throw err;
-            terminal.close();
-          }
-            console.log("Success.");
-            terminal.close();
-        });
-      });
+        console.log("Success. File was movied to the directory " + args[1]);
     });
-  } else {
-    console.log("Please, check the input data");
-    terminal.close();
-  }
-})
+  });
+});
