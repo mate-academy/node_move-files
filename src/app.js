ad65b216819235fc567473757469c31402497353
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-12-01 14:12:57 +0200

    Fixes-2 after review

--- src/app.js
+++ src/app.js
@@ -3,28 +3,27 @@
 import readline from 'readline';
 import fs from 'fs';
 
-const args = process.argv.slice(3);
+const [ file, folder ] = [...process.argv.slice(3)];
 
-if (args[1][args[1].length - 1] !== '/') {
-  console.log(args[1]);
+if (folder[folder.length - 1] !== '/') {
   throw new Error("The name of destination directory should be ended with '/'");
 }
 
-fs.readFile(args[0], (err, content) => {
+fs.readFile(file, (err, content) => {
   if (err) {
     throw err;
   }
 
-  fs.writeFile(args[1] + "/" + args[0], content, (err, data) => {
+  fs.writeFile(folder + "/" + file, content, (err, data) => {
     if (err) {
       throw err;
     }
 
-    fs.unlink(args[0], (err) => {
+    fs.unlink(file, (err) => {
       if (err) {
         throw err;
       }
-        console.log("Success. File was movied to the directory " + args[1]);
+        console.log("Success. File was moved to the directory " + folder);
     });
   });
 });
