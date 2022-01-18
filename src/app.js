f0adbb6f2c61608e371c55830459b7e2a000afb7
Roman Moroz <65686226+morozit@users.noreply.github.com>
2022-01-18 16:24:15 +0200

    develop

--- src/app.js
+++ src/app.js
@@ -1,21 +1,31 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+const copy = require('./copy');
+
+const fileCut = process.argv[2];
+const fileInsert = process.argv[3];
+const folderCheck1 = fileInsert[fileInsert.length - 1] === '/';
+const folderCheck2 = fs.existsSync(`src/${fileInsert}`);
+
+if (folderCheck1 || folderCheck2) {
+  const data = copy(fileCut);
+  const newPath = folderCheck1
+    ? `src/${fileInsert}${fileCut}`
+    : `src/${fileInsert}/${fileCut}`;
+
+  fs.writeFileSync(newPath, data);
+  fs.rmSync(`src/${fileCut}`);
+  // eslint-disable-next-line no-console
+  console.log('File moved');
+
+  return;
 }
 
-module.exports = sum;
+fs.rename(`src/${fileCut}`, `src/${fileInsert}`, (error) => {
+  if (error) {
+    throw error;
+  }
+  // eslint-disable-next-line no-console
+  console.log('File moved');
+});
