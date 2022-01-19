40f7b7cb065bc38a2e049ba8641230a1cc9ade70
Roman Moroz <65686226+morozit@users.noreply.github.com>
2022-01-19 14:10:06 +0200

    new answer

--- src/app.js
+++ src/app.js
@@ -1,31 +1,58 @@
 'use strict';
 
 const fs = require('fs');
-const copy = require('./copy');
 
 const fileCut = process.argv[2];
 const fileInsert = process.argv[3];
-const folderCheck1 = fileInsert[fileInsert.length - 1] === '/';
-const folderCheck2 = fs.existsSync(`src/${fileInsert}`);
 
-if (folderCheck1 || folderCheck2) {
-  const data = copy(fileCut);
-  const newPath = folderCheck1
-    ? `src/${fileInsert}${fileCut}`
-    : `src/${fileInsert}/${fileCut}`;
-
-  fs.writeFileSync(newPath, data);
-  fs.rmSync(`src/${fileCut}`);
-  // eslint-disable-next-line no-console
-  console.log('File moved');
-
-  return;
-}
-
-fs.rename(`src/${fileCut}`, `src/${fileInsert}`, (error) => {
+fs.stat(`${fileCut}`, (error, status) => {
   if (error) {
-    throw error;
+    // eslint-disable-next-line no-console
+    console.log('Ви вказали не правильний шлях звідки хочете вирізати файли');
   }
-  // eslint-disable-next-line no-console
-  console.log('File moved');
+
+  fs.stat(`${fileInsert}`, (errer, stats) => {
+    if (errer) {
+      // eslint-disable-next-line no-console
+      console.log('Змінює назву файла на назву');
+
+      fs.rename(`${fileCut}`, `${fileInsert}`, (err) => {
+        if (err) {
+          throw err;
+        }
+        // eslint-disable-next-line no-console
+        console.log('File rename: ', '>>', fileInsert, '<<');
+      });
+
+      return;
+    }
+
+    if (stats.isDirectory()) {
+      const contentFileCut = fs.readFile(fileCut, 'utf8', (err, data) => {
+        if (err) {
+          // eslint-disable-next-line no-console
+          console.log(err);
+        }
+      });
+      const destArr = fileCut.split('/');
+      const nameFile = destArr[1];
+
+      // eslint-disable-next-line max-len
+      fs.appendFile(`${fileInsert}${nameFile}`, `${contentFileCut}`, (err1, data) => {
+        if (err1) {
+          // eslint-disable-next-line no-console
+          console.log('не копіює з шляхом папка/');
+          throw err1;
+        }
+
+        fs.unlink(fileCut, (errorUnlink) => {
+          if (errorUnlink) {
+            throw errorUnlink;
+          }
+          // eslint-disable-next-line no-console
+          console.log('File moved');
+        });
+      });
+    }
+  });
 });
