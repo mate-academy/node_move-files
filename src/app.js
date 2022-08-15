40fc0b316735a6ff65452932b69a8748c67147e3
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-08-15 15:40:32 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,41 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
+const readLine = require('readline');
 
-module.exports = sum;
+const terminal = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const questionForUser = (question) => {
+  return new Promise((resolve) => {
+    terminal.question(question, (answer) => {
+      if (!answer.length) {
+        console.log('Please, enter something');
+
+        return questionForUser(question);
+      } else {
+        resolve(answer);
+      }
+    });
+  });
+};
+
+const moveFile = async() => {
+  const originalName = await questionForUser('Choose a file to move: ');
+  const newName = await questionForUser('Choose a new name and path: ');
+
+  fs.rename(`${originalName}`, `${newName}`, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('You have successfully moved the file');
+    }
+  });
+
+  terminal.close();
+};
+
+moveFile();
