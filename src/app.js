b23fb7300a9074c3ad8598be5f1af272bfa450d2
Usenko Bohdana <usenkobogdana19@gmail.com>
2022-10-12 14:50:06 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+
+const [ file, folder ] = [...process.argv.slice(3)];
+
+if (folder[folder.length - 1] !== '/') {
+  throw new Error('The name of destination directory should be ended with /');
 }
 
-module.exports = sum;
+fs.readFile(file, (err, content) => {
+  if (err) {
+    throw err;
+  }
+
+  fs.writeFile(folder + '/' + file, content, (err1) => {
+    if (err1) {
+      throw err1;
+    }
+
+    fs.unlink(file, (err2) => {
+      if (err2) {
+        throw err2;
+      }
+
+      // eslint-disable-next-line no-console
+      console.log('Success. File was moved to the directory ' + folder);
+    });
+  });
+});
