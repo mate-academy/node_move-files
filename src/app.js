c617447a2a16194e1ff48a5c628773aa27fc6f0a
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-10-15 18:58:15 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+
+function mv() {
+  const [sourse, destination] = process.argv.slice(2);
+  const isDir = destination.slice(-1) === '/';
+  const finalDestination = isDir
+    ? destination + sourse.split('/').slice(-1)
+    : destination + '/' + sourse.split('/').slice(-1);
+
+  try {
+    fs.copyFileSync(sourse, finalDestination);
+    fs.rmSync(sourse);
+  } catch (error) {
+    if (error.code === 'ENOENT' && !isDir) {
+      fs.copyFileSync(sourse, destination);
+      fs.rmSync(sourse);
+    } else {
+      throw error;
+    }
+  }
 }
 
-module.exports = sum;
+mv();
