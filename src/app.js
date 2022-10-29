25fd152b3ee691a8d3359d194096875c48e1d389
Max Bovtun <maxbovtun@gmail.com>
2022-10-29 15:55:31 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,48 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+const path = require('path');
+
+function mv(fileFrom, fileTo) {
+  if (fileFrom === fileTo) {
+    return;
+  }
+
+  const basePath = path.join(__dirname, fileFrom);
+  const content = fs.readFileSync(basePath, 'utf-8');
+
+  try {
+    fs.writeFileSync(fileTo, content);
+    fs.unlinkSync(basePath);
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      throw new Error(e);
+    } else if (e.code === 'EISDIR') {
+      try {
+        const newPath = fileTo[fileTo.length - 1] === '/'
+          ? fileTo + fileFrom
+          : fileTo;
+
+        fs.writeFileSync(newPath, content);
+        fs.unlinkSync(basePath);
+      } catch (err) {
+        if (err.code === 'EISDIR') {
+          try {
+            const newPath = fileTo + '/' + fileFrom;
+
+            fs.writeFileSync(newPath, content);
+            fs.unlinkSync(basePath);
+          } catch (error) {
+            throw new Error(error);
+          }
+        } else {
+          throw new Error(err);
+        }
+      }
+    }
+  }
 }
 
-module.exports = sum;
+mv('test.txt', 'existingDir/test.txt');
+
+module.exports = mv;
