2d4e9a8682fb9a965c9f7814d4bf07a42ac8f6d9
ZUBOV ILLIA <zubovillia@gmail.com>
2022-11-06 13:11:30 +0300

    lgtm

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
+const path = require('path');
 
-module.exports = sum;
+const mv = (fileToMove, dest) => {
+  try {
+    const file = fs.readFileSync(path.join(__dirname, fileToMove));
+
+    fs.writeFileSync(path.join(__dirname, dest), file, 'utf-8');
+
+    fs.rm(path.join(__dirname, fileToMove), err => {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+mv('./test/file.txt', './test/folder/new_name.txt');
+
+module.exports = { mv };
