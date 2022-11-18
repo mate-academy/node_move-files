af586f8521c9ab6609185eeab63b2269a7ac7ddd
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-11-18 21:49:04 +0200

    Implemented move file app

--- src/app.js
+++ src/app.js
@@ -1,21 +1,56 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
+const path = require('path');
 
-module.exports = sum;
+const moveFile = (file, moveTo) => {
+  const pathToMove = path.join(__dirname, moveTo);
+  const currFilePath = path.join(__dirname, file);
+
+  const isEndsWithSlash = moveTo.endsWith('/');
+  let isDirectory = false;
+
+  if (currFilePath === pathToMove) {
+    return;
+  }
+
+  if (!fs.existsSync(currFilePath)) {
+    throw new Error('The file does not exist!');
+  }
+
+  if (fs.existsSync(pathToMove)) {
+    const stats = fs.statSync(pathToMove);
+
+    isDirectory = stats.isDirectory();
+  }
+
+  if (isEndsWithSlash && !isDirectory) {
+    throw new Error('The directory does not exist!');
+  }
+
+  if (!isDirectory && !isEndsWithSlash) {
+    try {
+      fs.renameSync(currFilePath, pathToMove);
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  if (isDirectory) {
+    const dirPath = (isEndsWithSlash) ? moveTo : moveTo + '/';
+    const newFilePath = path.join(__dirname, dirPath + file);
+
+    try {
+      const data = fs.readFileSync(currFilePath, 'utf-8');
+
+      fs.writeFileSync(newFilePath, data);
+      fs.unlinkSync(currFilePath);
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+};
+
+moveFile('test.txt', 'test');
+
+module.exports = { moveFile };
