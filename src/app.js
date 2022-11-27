04d051336a8760b7e32d000302d06605404cb9a0
Misha Dzhemula <medvedmisha23@gmail.com>
2022-11-27 15:42:12 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,50 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+
+const file = process.argv[2];
+const dest = process.argv[3];
+let destDir = dest;
+let destFile = '';
+let checked = false;
+
+function setDestAsFile() {
+  destDir = './';
+  destFile = dest;
 }
 
-module.exports = sum;
+if (!dest.endsWith('/')) {
+  if (dest.includes('/')) {
+    const splitedDest = dest.split('/');
+
+    destDir = splitedDest.slice(0, -1).join('/') + '/';
+    destFile = splitedDest[splitedDest.length - 1];
+  } else if (dest.includes('.')) {
+    setDestAsFile();
+    checked = true;
+  } else {
+    checked = true;
+
+    try {
+      if (fs.statSync(dest).isDirectory()) {
+        destDir = `./${dest}/`;
+      }
+    } catch (_err) {
+      setDestAsFile();
+    }
+  }
+}
+
+if (!checked) {
+  try {
+    fs.statSync(destDir);
+  } catch (err) {
+    throw new Error('no such directory', err);
+  }
+}
+
+if (!destFile) {
+  destFile = file;
+}
+
+fs.renameSync(file, destDir + destFile);
