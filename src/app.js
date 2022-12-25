95bc97b4ecfd8f9a7e900fe2c1b95c3031f245b4
Ievgen Volkov <neoodessa86@gmail.com>
2022-12-25 21:51:03 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,50 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
 
-module.exports = sum;
+const [currentFile, path] = process.argv.slice(2);
+
+fs.stat(path, (statErr, stats) => {
+  if (statErr && statErr.code === 'ENOENT') {
+    fs.rename(currentFile, path, (renameErr) => {
+      if (renameErr) {
+        // eslint-disable-next-line no-console
+        console.log(`Such directory '${path}' does not exist.`);
+
+        return;
+      }
+
+      // eslint-disable-next-line no-console
+      console.log(
+        // eslint-disable-next-line max-len
+        `The file '${currentFile}' was successfully renamed and moved to the '${path}' directory.`
+      );
+    });
+
+    return;
+  }
+
+  if (stats.isDirectory()) {
+    fs.copyFile(currentFile, `${path}/${currentFile}`, (copyErr) => {
+      if (copyErr) {
+        // eslint-disable-next-line no-console
+        console.log(copyErr);
+
+        return;
+      }
+
+      fs.unlink(currentFile, (unlinkErr) => {
+        if (unlinkErr) {
+          // eslint-disable-next-line no-console
+          console.log(unlinkErr);
+        }
+      });
+
+      // eslint-disable-next-line no-console
+      console.log(
+        // eslint-disable-next-line max-len
+        `The file '${currentFile}' was successfully copied to '${path}/${currentFile}'.`
+      );
+    });
+  }
+});
