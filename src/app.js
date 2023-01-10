e1c2cb0ba6f0240ffa9a4292cc6a7bce01d189db
taras.holiuk <taras.holiuk@vestr.com>
2023-01-10 16:17:19 +0200

    create move file app

--- src/app.js
+++ src/app.js
@@ -1,21 +1,57 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+const path = require('path');
+
+const [enteredSourcePath, enteredPath] = process.argv.slice(2);
+
+const resolvedPath = path.resolve(__dirname, '../', enteredPath);
+
+const resolvedSourcePath = path.resolve(__dirname, '../', enteredSourcePath);
+const fileName = enteredSourcePath.split('/').slice(-1);
+
+
+const moveFile = (sourceFilePath, destinationFilePath) => {
+  try {
+    fs.copyFileSync(sourceFilePath, destinationFilePath);
+  } catch (error) {
+    throw new Error(`An error ocurred during copying file: ${error}`);
+  }
+
+  try {
+    fs.rmSync(sourceFilePath)
+  } catch (error) {
+    throw new Error(`An error ocurred during removing the old file: ${error}`);
+  }
+
+  console.info('File successfully moved');
 }
 
-module.exports = sum;
+const getStats = (path) => {
+  try {
+    const stats = fs.statSync(path);
+    return stats;
+  } catch (_e) {
+    console.info('Invalid path entered. Trying to rename file...');
+  }
+}
+
+const renameFile = (fileToRename, newFileName) => {
+  try {
+    fs.renameSync(fileToRename, newFileName);
+  } catch (error) {
+    console.error('An error ocurred during renaming file:', error);
+  }
+
+  console.info('File successfully renamed');
+}
+
+const stats = getStats(resolvedPath);
+
+if (stats && stats.isDirectory()) {
+  moveFile(resolvedSourcePath, `${resolvedPath}/${fileName}`)
+} else if (enteredPath.slice(-1) === '/') {
+  throw new Error('Invalid destination path')
+} else {
+  renameFile(resolvedSourcePath, `${resolvedPath}`)
+}
