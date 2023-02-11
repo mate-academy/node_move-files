296c4ca53b5ea075aa73ae03d3ba3975e2a56eac
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-02-11 18:01:00 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,45 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function moveFile(src, dest) {
+  let destination = dest;
+  const destIsDirectory = destination.endsWith('/');
+  const destDir = path.dirname(destination);
+
+  if (!fs.existsSync(src)) {
+    throw new Error(`Source file ${src} does not exist`);
+  }
+
+  if (destIsDirectory) {
+    destination = path.join(destination, path.basename(src));
+  }
+
+  if (!fs.existsSync(destDir)) {
+    throw new Error(`Destination directory ${destDir} does not exist`);
+  }
+
+  fs.renameSync(src, destination);
 }
 
-module.exports = sum;
+terminal.question('Enter the source file path: ', (src) => {
+  terminal.question('Enter the destination file path: ', (dest) => {
+    try {
+      moveFile(src, dest);
+      // eslint-disable-next-line no-console
+      console.log(`Moved ${src} to ${dest}`);
+      terminal.close();
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error.message);
+      terminal.close();
+    }
+  });
+});
