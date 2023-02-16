3e96fa66a042ab8a4336eba3bdb75cbac91eab42
Lefteris Loizides <loizides.lef@gmail.com>
2023-02-16 16:44:48 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs/promises');
+const path = require('path');
 
-module.exports = sum;
+// eslint-disable-next-line no-shadow
+const move = async(file, newPath) => {
+  let np = newPath;
+
+  try {
+    const stats = await fs.stat(np);
+    const endpoint = (/.+\/$/.test(np) ? '' : '/') + path.basename(file);
+
+    if (stats.isDirectory()) {
+      np += endpoint;
+    }
+
+    await fs.rename(file, np);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+};
+
+const [file, newPath] = process.argv.slice(2);
+
+move(file, newPath);
