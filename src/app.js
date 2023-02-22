51c4b86b4510f15964adabb5f73fba7b8551e543
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-02-22 20:35:00 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+const { basename } = require('path');
+
+const [source, target] = process.argv.slice(2);
+let newTarget;
+
+fs.existsSync(target)
+  ? newTarget = target + '/'
+  : newTarget = target;
+
+const isDirectory = newTarget.endsWith('/');
+
+if (isDirectory) {
+  newTarget = newTarget + basename(source);
 }
 
-module.exports = sum;
+console.log(newTarget);
+
+fs.rename(source, newTarget, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Moved ${source} to ${target}`);
+  }
+});
