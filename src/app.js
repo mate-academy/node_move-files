d2260b5d44d37b8151265939e80e89f43014674d
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-03-15 08:11:51 +0200

    made small improvement

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const { terminal } = require('./terminal');
 const { moveFile } = require('./moveFile');
 
 terminal.question(
-  // eslint-disable-next-line max-len
-  'Enter the command to move the file to another directory.\nFor example, "mv file.txt ./someDir/" ',
+  'Enter the command to move the file to another directory.'
+  + '\nFor example, "mv file.txt ./someDir/" ',
   moveFile,
 );
