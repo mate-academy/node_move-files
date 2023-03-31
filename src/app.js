8689a461a11734e2052d3f7fc254857cd207bf52
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-03-31 15:57:55 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,7 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const move = require('./move');
 
-module.exports = sum;
+const [fromPath, toPath] = process.argv.slice(2);
+
+move.moveFile(fromPath, toPath);
