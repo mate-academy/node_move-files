96d8a23e93ae4b40e8dcea6c7ca3803963c16c93
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-04-25 15:27:53 +0300

    move files

--- src/app.js
+++ src/app.js
@@ -1,21 +1,64 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+const path = require('path');
+
+const [paramFile, paramDestination] = process.argv.slice(2);
+
+const green = '\x1b[32m';
+const red = '\x1b[31m';
+
+const logs = {
+  arguments: () => {
+    console.log(
+      `${red}You need to write the old file name and the new path. \x1b[0m`
+       + `\nLike ${green}node ./src/moveFile.js file.txt ./someDir/`
+    );
+  },
+  directory: () => {
+    console.log(
+      `${red}The directory does not exist`
+    );
+  },
+  error: () => {
+    console.log(
+      `${red}Something went wrong. Try again!`
+    );
+  },
+  success: (destinationPath) => {
+    console.log(
+      `${green}Success!`
+      + `\nMoved to: ${destinationPath}`
+    );
+  },
+};
+
+function moveFile(file, destination) {
+  if (!file || !destination) {
+    logs.arguments();
+
+    return;
+  }
+
+  const filePath = path.join(__dirname, file);
+  const destinationPath = path.join(__dirname, destination);
+  const fileName = path.basename(filePath);
+  const isValid = destination.endsWith('/');
+
+  if (isValid && !fs.existsSync(destinationPath)) {
+    logs.directory();
+  }
+
+  try {
+    fs.renameSync(
+      filePath,
+      isValid ? path.join(destinationPath, fileName) : destinationPath,
+    );
+    logs.success(destinationPath);
+  } catch (err) {
+    logs.error();
+  }
 }
 
-module.exports = sum;
+moveFile(paramFile, paramDestination);
