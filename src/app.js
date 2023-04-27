0e2dca5d853cbf384eee951985943088ec8b330c
Valeriia Yarzhemska <lera.strikalo@icloud.com>
2023-04-28 01:09:45 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,50 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+const path = require('path');
+
+function moveFileToDirectory() {
+  const [src, dest] = process.argv.slice(2);
+
+  if (process.argv.length < 4) {
+    console.log('Please, enter correct file name and destination to move');
+
+    return;
+  }
+
+  const oldPath = path.join(__dirname, src);
+  const newPath = path.join(__dirname, dest);
+  const isDirectory = dest.endsWith('/');
+  const newDestination = isDirectory
+    ? path.join(newPath, path.basename(oldPath))
+    : newPath;
+
+  if (!fs.existsSync(oldPath)) {
+    console.error('Source file doesnt exist');
+
+    return;
+  }
+
+  if (oldPath === newPath) {
+    console.log('You cant move file to the same folder');
+
+    return;
+  }
+
+  if (!fs.existsSync(newPath) && isDirectory) {
+    console.log('This directory doesnt exist');
+
+    return;
+  }
+
+  fs.rename(oldPath, newDestination, (error) => {
+    if (error) {
+      console.log(error);
+    }
+
+    console.log('Operation was done');
+  });
 }
 
-module.exports = sum;
+moveFileToDirectory();
