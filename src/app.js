c889b13f7db95ff392bc9e26a6bbd8cfba1f3d12
Valeriia Yarzhemska <lera.strikalo@icloud.com>
2023-05-07 13:24:32 +0300

    fixed comments

--- src/app.js
+++ src/app.js
@@ -3,46 +3,59 @@
 
 const fs = require('fs');
 const path = require('path');
+const { terminal } = require('./terminal');
 
-function moveFileToDirectory() {
-  const [sourcePath, destinationPath] = process.argv.slice(2);
+const [,, sourcePath, destinationPath] = process.argv;
 
+const moveFileToDirectory = (source, destination) => {
   if (process.argv.length < 4) {
     console.log('Please, enter correct file name and destination to move');
+    start();
 
     return;
   }
 
-  const isDirectory = destinationPath.endsWith('/');
+  const isDirectory = destination.endsWith('/');
   const newDestinationPath = isDirectory
-    ? path.join(destinationPath, path.basename(sourcePath))
-    : destinationPath;
+    ? path.join(destination, path.basename(source))
+    : destination;
 
-  if (!fs.existsSync(sourcePath)) {
+  if (!fs.existsSync(source)) {
     console.error('Source file doesnt exist');
+    start();
 
     return;
   }
 
-  if (sourcePath === newDestinationPath) {
+  if (source === newDestinationPath) {
     console.error('You cant move file to the same folder');
+    start();
 
     return;
   }
 
   if (!fs.existsSync(newDestinationPath) && isDirectory) {
     console.error('This directory doesnt exist');
+    start();
 
     return;
   }
 
-  fs.rename(sourcePath, newDestinationPath, (error) => {
+  fs.rename(source, newDestinationPath, (error) => {
     if (error) {
       console.error(error);
     }
 
     console.log('Operation was done');
+    terminal.close();
   });
-}
+};
 
-moveFileToDirectory();
+const start = () => {
+  terminal.question(
+    'To move a file u need to write: "move /file-to-copy/ /new-file/" ',
+    () => moveFileToDirectory(sourcePath, destinationPath),
+  );
+};
+
+start();
