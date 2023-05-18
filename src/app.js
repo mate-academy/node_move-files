f4471c782f4bde55560addb074efd2b60d814b2e
Olga Bordiug <obordiug@gmail.com>
2023-05-18 15:05:09 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
+const path = require('path');
 
-module.exports = sum;
+const moveFile = (filePath, destPath) => {
+  const fileName = path.basename(filePath);
+  const destDir = path.dirname(destPath);
+  const isDestDir = destPath.endsWith('/');
+  const newFilePath = isDestDir ? path.join(destPath, fileName) : destPath;
+
+  if (!fs.existsSync(destDir)) {
+    console.error('Destination directory does not exist');
+
+    return;
+  }
+
+  fs.rename(filePath, newFilePath, error => {
+    if (error) {
+      console.error(`Error while moving file: ${error}`);
+    } else {
+      console.log(`Moved ${filePath} to ${newFilePath}`);
+    }
+  });
+};
+
+const [file, dest] = process.argv(2);
+
+moveFile(file, dest);
