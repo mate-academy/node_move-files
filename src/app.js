af14a7de6398d5061e99c102972c1116d6efbac0
Vladimir Yurkov <vladimir.yurkov.91@gmail.com>
2023-05-21 20:40:26 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+const path = require('path');
+
+const sourcePath = process.argv[2];
+const destinationPath = process.argv[3];
+
+function moveFile(sourceFilePath, destinationFilePath) {
+  const source = path.resolve(sourceFilePath);
+  let destination = path.resolve(destinationFilePath);
+
+  if (!fs.existsSync(source)) {
+    throw new Error('Source path does not exist!');
+  }
+
+  if (!fs.statSync(source).isFile()) {
+    throw new Error('Source is not a file!');
+  }
+
+  if (destinationFilePath.endsWith('/')) {
+    if (!fs.existsSync(destination)) {
+      throw new Error('Destination path already exists!');
+    }
+
+    destination = path.join(destination, path.basename(sourceFilePath));
+  }
+
+  try {
+    fs.renameSync(source, destination);
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
-module.exports = sum;
+moveFile(sourcePath, destinationPath);
