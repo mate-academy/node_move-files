2685e2557ebb6d631f78dad21124de891c5dc3d3
Illia Kyrychenko <i.kirichenko160804@gmail.com>
2023-06-30 01:49:55 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
 
-module.exports = sum;
+const renameFile = (oldName, newName, isExist) => {
+  const oldPath = `src/${oldName}`;
+  const newPath = isExist ? `src/${newName}/${oldName}` : `src/${newName}`;
+
+  fs.rename(oldPath, newPath, (err) => {
+    if (err) {
+      throw new Error('Rename error');
+    }
+  });
+};
+
+const app = () => {
+  const args = process.argv.slice(2);
+  const firstPath = args[0];
+  const secondPath = args[1];
+
+  try {
+    const isExists = fs.existsSync(`src/${secondPath}`);
+
+    if (secondPath[secondPath.length - 1] === '/' && !isExists) {
+      throw new Error('No such directory');
+    }
+
+    renameFile(firstPath, secondPath, isExists);
+  } catch (error) {
+    // Some error handling
+  }
+};
+
+app();
+
+module.exports = { app };
