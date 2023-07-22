77e323122b2f35f2dd2223d5eb8ea9dc296a2cdb
Andrij Nalyvayko <andrij.nalyvayko@gmail.com>
2023-07-22 12:50:13 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs/promises');
-const { sep, basename } = require('path');
+const { sep, basename, dirname } = require('path');
 
 const fileParam = process.argv[2];
 const endpointParam = process.argv[3];
@@ -10,6 +10,14 @@ moveFile(fileParam, endpointParam);
 
 async function moveFile(file, endpoint) {
   try {
+    const isOnTheSameDir = dirname(fileParam) === dirname(endpointParam);
+
+    if (isOnTheSameDir) {
+      await fs.rename(fileParam, endpointParam);
+
+      return;
+    }
+
     const dataOfFile = await fs.readFile(file);
 
     await fs.writeFile(endpoint, dataOfFile);
@@ -22,5 +30,8 @@ async function moveFile(file, endpoint) {
     if (err.code === 'EISDIR') {
       return moveFile(file, endpoint + sep + basename(file));
     }
+
+    // eslint-disable-next-line no-console
+    console.log('ERROR: ', err.message);
   }
 }
