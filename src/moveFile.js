88a4345750399410c71f0f05f3fc59383c631e2d
Mykhailo Kostov <kostov.mykhailo@gmail.com>
2023-08-03 20:06:13 +0300

    Solution

---
+++ src/moveFile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function moveFile(fileName, moveTo) {
+  let movePath = path.join(__dirname, '..', moveTo);
+
+  movePath = movePath.endsWith('/') ? movePath.slice(0, -1) : movePath;
+
+  const isDir = fs.existsSync(movePath);
+  const filePath = path.join(__dirname, fileName);
+  const data = fs.readFileSync(filePath, 'utf-8');
+
+  if (!isDir) {
+    throw new Error(`Directory ${movePath} does not exist`);
+  }
+
+  movePath = path.join(movePath, fileName);
+
+  fs.writeFileSync(movePath, data);
+  fs.rmSync(filePath);
+};
+
+module.exports = moveFile;
