21febea3dfd64a38690ede96df4f4b1dc11e8ab3
MaxSchmide <kuznetsov.max.v@gmail.com>
2023-08-05 00:11:11 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,46 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
+const path = require('path');
 
-module.exports = sum;
+function moveFile() {
+  const args = process.argv.splice(2);
+  const [source, dest] = args;
+
+  if (!source || !dest) {
+    throw new Error('File name and destination are required!');
+  }
+
+  const normalizeSource = path.join(__dirname, source);
+  const normalizeDestination = path.join(__dirname, dest);
+
+  try {
+    const data = fs.readFileSync(normalizeSource, 'utf8');
+
+    if (dest.endsWith(source.split('.')[1])) {
+      fs.renameSync(normalizeSource, normalizeDestination);
+
+      return;
+    }
+
+    if (!fs.existsSync(normalizeDestination)) {
+      throw new Error('No such directory');
+    }
+
+    const sourceArr = source.split('/');
+    const filename = sourceArr[sourceArr.length - 1];
+
+    fs.rmSync(normalizeSource);
+
+    fs.writeFileSync(
+      path.join(normalizeDestination, filename),
+      data,
+      'utf8',
+    );
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+moveFile();
