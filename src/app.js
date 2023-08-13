cf514de936aef4dead447bd7368d124bf561760c
alex <cebanoleksandr@gmail.com>
2023-08-13 17:03:18 +0300

    fix bug

--- src/app.js
+++ src/app.js
@@ -2,17 +2,4 @@
 
 const { moveFiles } = require('./moveFiles');
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
 moveFiles(process.argv[2], process.argv[3]);
--- src/moveFiles.js
+++ src/moveFiles.js
@@ -1,22 +1,29 @@
+/* eslint-disable no-console */
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 function moveFiles(firstPath, secondPath) {
-  const firstFile = firstPath.split('/')[firstPath.split('/').length - 1];
-  let newPath = '';
+  const folder = path.dirname(secondPath);
 
-  if (secondPath[secondPath.length - 1] === '/') {
-    newPath = secondPath + firstFile;
+  if (!fs.existsSync(folder)) {
+    console.log(`Folder ${folder} does not exists.`);
+
+    return;
   }
 
-  if (fs.existsSync(secondPath)) {
-    newPath = secondPath + '/' + firstFile;
+  if (!fs.existsSync(firstPath)) {
+    console.log(`File ${firstPath} does not exists.`);
+
+    return;
   }
 
-  fs.rename(firstPath, newPath, (err) => {
+  fs.rename(firstPath, secondPath, (err) => {
     if (err) {
       throw new Error(err);
+    } else {
+      console.log(`${firstPath} has been moved to ${folder}`);
     }
   });
 };
