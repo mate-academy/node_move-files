451259c63d74a254e969687a4916fe73c550e363
Mariia <mariia.monakhova1@gmail.com>
2023-08-14 18:35:11 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,8 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const { moveFiles } = require('./moveFiles');
 
-module.exports = sum;
+const sourceFile = process.argv[2];
+const destinationPath = process.argv[3];
+
+moveFiles(sourceFile, destinationPath);
---
+++ src/moveFiles.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+'use strict';
+
+const fs = require('fs');
+
+function moveFiles(source, destination) {
+  if (!fs.existsSync(source)) {
+    console.log('This file does not exists');
+
+    return;
+  }
+
+  const sourceStat = fs.statSync(source);
+
+  if (!sourceStat.isFile()) {
+    console.log('This is not a file');
+
+    return;
+  }
+
+  if (destination.endsWith('/')) {
+    const destinationDir = destination;
+
+    if (!fs.existsSync(destinationDir)) {
+      console.log('The directory does not exist');
+    }
+
+    try {
+      fs.renameSync(source, destination + source);
+      console.log('Moved successfully');
+    } catch (error) {
+      console.log(`Cannot move file \n ${error}`);
+    }
+  } else {
+    if (!destination.includes('/') && !fs.existsSync(destination)) {
+      fs.renameSync(source, `./${destination}.txt`);
+      console.log('Renamed successfully');
+    } else if (!destination.includes('/') && fs.existsSync(destination)) {
+      try {
+        fs.renameSync(source, `./${destination}/${source}`);
+        console.log('Moved successfully');
+      } catch (error) {
+        console.log(`Cannot move file \n ${error}`);
+      }
+    } else {
+      try {
+        fs.renameSync(source, destination);
+        console.log('Moved successfully');
+      } catch (error) {
+        console.log(`Cannot move file \n ${error}`);
+      }
+    }
+  }
+}
+
+module.exports = { moveFiles };
