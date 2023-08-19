ff572e9ea36ce5a40de9d6d7fa934a1fb379eac4
Boiko Vlad <bojko.vladislav16@gmail.com>
2023-08-19 14:41:27 +0300

    Changes after code review

--- src/app.js
+++ src/app.js
@@ -10,17 +10,15 @@ const destinationFolder = process.argv[3];
 const sourceFileName = path.basename(sourcePath);
 const destinationFolderName = path.basename(destinationFolder);
 const destinationPath = path.join(destinationFolder, sourceFileName);
+const isDestinationExists = fs.existsSync(destinationFolder);
 
-fs.existsSync(destinationFolder, (err) => {
-  if (err) {
-    throw new Error('Destination folder does not exist');
-  }
-});
+if (!isDestinationExists) {
+  console.error('Destination folder does not exist');
+}
 
-fs.rename(sourcePath, destinationPath, (err) => {
-  if (err) {
-    throw new Error('Failed to move file');
-  } else {
-    console.log(`The file ${sourceFileName} has been moved to ${destinationFolderName} folder`);
-  }
-});
+try {
+  fs.renameSync(sourcePath, destinationPath);
+  console.log(`The file ${sourceFileName} has been moved to ${destinationFolderName} folder`);
+} catch (error) {
+  throw new Error('Failed to move file');
+}
