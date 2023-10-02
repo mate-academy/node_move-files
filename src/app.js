306ce170ed112714112b81332d62ab440f6d488b
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-10-03 00:55:23 +0300

    move file implemented

--- src/app.js
+++ src/app.js
@@ -1,21 +1,46 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const path = require('path');
+const fs = require('fs');
+const { moveFile } = require('./moveFile.js');
+const params = process.argv.slice(2);
+
+if (params.length !== 2) {
+  console.log('Incorrect parameters');
+
+  return;
 }
 
-module.exports = sum;
+const addressFile = path.join(__dirname, params[0]);
+const addressMove = path.join(__dirname, params[1]);
+const file = params[0].split('/').slice(-1);
+
+if (!fs.existsSync(addressFile)) {
+  console.log('File not found');
+
+  return;
+}
+
+if (params[1].includes('/')) {
+  if (params[1].lastIndexOf('/') === params[1].length - 1) {
+    moveFile(addressFile, path.join(addressMove, file[0]));
+  } else {
+    moveFile(addressFile, addressMove);
+  }
+
+  return;
+}
+
+if (!params[1].includes('.')) {
+  try {
+    fs.readdirSync(addressMove);
+    moveFile(addressFile, path.join(addressMove, `/${file[0]}`));
+  } catch (err) {
+    moveFile(addressFile, addressMove);
+  };
+
+  return;
+}
+
+moveFile(addressFile, addressMove);
