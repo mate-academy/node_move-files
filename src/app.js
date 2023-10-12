c61bb53911b71d265efeab32405d64c731224302
KatOlista^^! <olya.katyushina@gmail.com>
2023-10-12 18:45:07 +0200

    implement an app that will move a file from one location to another

--- src/app.js
+++ src/app.js
@@ -1,21 +1,11 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const { removeFile } = require('./removeFile');
 
-module.exports = sum;
+const getAddress = () => {
+  const [from, to] = process.argv.slice(2);
+
+  removeFile(from, to);
+};
+
+getAddress();
