0d5bc66ae73bdd2d9e393423a0eab794f9f47b5a
KatOlista^^! <olya.katyushina@gmail.com>
2023-10-12 19:25:31 +0200

    delete redundant file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
+/* eslint-disable no-console */
 'use strict';
 
-const { removeFile } = require('./removeFile');
+const fs = require('fs');
+const { SLESH } = require('./constants');
 
-const getAddress = () => {
+const moveFile = () => {
   const [from, to] = process.argv.slice(2);
 
-  removeFile(from, to);
+  if (from === to) {
+    return;
+  }
+
+  if (!to.includes(SLESH) && to.includes('.txt')) {
+    fs.renameSync(from, to);
+  }
+
+  if (to.includes(SLESH) && to[to.length - 1] !== SLESH) {
+    fs.copyFileSync(from, to);
+    fs.rmSync(from);
+  }
+
+  if (to[to.length - 1] === SLESH) {
+    if (fs.existsSync(to)) {
+      fs.copyFileSync(from, `${to}${from}`);
+      fs.rmSync(from);
+    } else {
+      console.log('Directory is not exist');
+    }
+  }
+
+  if (!to.includes('.txt') && !to.includes(SLESH)) {
+    if (fs.existsSync(to)) {
+      fs.copyFileSync(from, `${to}${SLESH}${from}`);
+      fs.rmSync(from);
+    } else {
+      fs.renameSync(from, `./${to}`);
+    }
+  }
 };
 
-getAddress();
+moveFile();
