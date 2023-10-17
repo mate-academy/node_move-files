a588d7c758438a91ccd2ff9fff583335413110cd
Andrzej Pietkiewicz <pietan@me.com>
2023-10-17 15:06:35 +0200

    Move solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,45 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
 
-module.exports = sum;
+const [command, from, to] = process.argv.slice(2);
+
+if (command === 'move' && from !== to) {
+  fs.readFile(from, (error, fileData) => {
+    if (error) {
+      console.log(error);
+    }
+
+    const fileName = from.slice(from.lastIndexOf('/') + 1);
+    let pathTo = to;
+
+    if (pathTo.slice(-1) === '/') {
+      pathTo += fileName;
+    }
+
+    if (pathTo.slice(0, 1) !== '.' || pathTo.slice(0, 2) !== './') {
+      pathTo = './' + pathTo;
+    }
+
+    if (pathTo.slice(-4, -3) !== '.') {
+      pathTo += fileName.slice(-4);
+    }
+
+    fs.writeFile(pathTo, fileData.toString(), (err) => {
+      if (err) {
+        console.error(err.code === 'ENOENT'
+          ? 'no such file or directory'
+          : err.code);
+
+        return;
+      }
+
+      fs.rm(from, rmError => {
+        if (rmError) {
+          console.error(rmError);
+        }
+      });
+    });
+  });
+}
