1fd8cf5134fc783d97834b07c89f53ce0aa1576a
Nikita <Nehrim24@gmail.com>
2023-10-25 00:28:34 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,52 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs');
+const path = require('path');
+
+function moveFile(sourcePath, destinationPath) {
+  if (!fs.existsSync(sourcePath)) {
+    // eslint-disable-next-line
+    console.error(`Source file '${sourcePath}' does not exist.`);
+
+    return;
+  }
+
+  const isDestinationDirectory = destinationPath.endsWith('/');
+  let finalDestinationPath = destinationPath;
+
+  if (isDestinationDirectory) {
+    const sourceFileName = path.basename(sourcePath);
+
+    finalDestinationPath = path.join(destinationPath, sourceFileName);
+  }
+
+  const destinationDir = path.dirname(finalDestinationPath);
+
+  if (!fs.existsSync(destinationDir)) {
+    // eslint-disable-next-line
+    console.error(`Destination directory '${destinationDir}' does not exist.`);
+
+    return;
+  }
+
+  fs.rename(sourcePath, finalDestinationPath, (err) => {
+    if (err) {
+      // eslint-disable-next-line
+      console.error(`Error moving '${sourcePath}' to
+      '${finalDestinationPath}': ${err.message}`);
+    } else {
+      // eslint-disable-next-line
+      console.log(`Moved '${sourcePath}' to '${finalDestinationPath}'.`);
+    }
+  });
 }
 
-module.exports = sum;
+const sourceFilePath = process.argv[2];
+const destinationFilePath = process.argv[3];
+
+if (!sourceFilePath || !destinationFilePath) {
+  // eslint-disable-next-line
+  console.error('Usage: node index.js sourcePath destinationPath');
+} else {
+  moveFile(sourceFilePath, destinationFilePath);
+}
