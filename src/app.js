a4644bc56a237ea0cd1fb5b0aa16a9d88b946aaa
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-11-04 22:26:27 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
+const fs = require('fs-extra');
+const path = require('path');
+
+function move(file, pathToMove) {
+  fs.move(file, pathToMove)
+    .then(() => console.log('Move succses'))
+    .catch(() => console.error('Error moving'));
 }
 
-module.exports = sum;
+const fileName = process.argv[2];
+const pathTo = process.argv[3];
+
+if (process.argv[4]) {
+  console.log('Only 2 param accept');
+} else {
+  fs.stat(pathTo, (err, stats) => {
+    if (err) {
+      if (pathTo[pathTo.length - 1] !== '/') {
+        move(fileName, pathTo);
+
+        return;
+      } else {
+        console.log('No such dir');
+
+        return;
+      }
+    }
+
+    if (stats.isDirectory()) {
+      move(fileName, path.join(pathTo, fileName));
+    }
+  });
+}
