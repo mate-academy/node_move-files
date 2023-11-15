96b7b77451158682370240f6bad94478367db044
Oleh <olezhkapopovych@gmail.com>
2023-11-15 14:04:56 +0200

    solution 1

--- src/app.js
+++ src/app.js
@@ -1,21 +1,48 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+/* eslint no-console: ["error", { allow: ["log"] }] */
 
-module.exports = sum;
+const fs = require('fs');
+const path = require('path');
+
+const copy = () => {
+  const args = process.argv.slice(2);
+  const sourcePath = path.join(path.join(process.cwd()), args[0]);
+  let destPath = path.join(path.join(process.cwd()), args[1]);
+
+  const lastBackslachIndex = sourcePath.lastIndexOf('\\');
+
+  const fileName = sourcePath.slice(lastBackslachIndex);
+
+  if (args[1].lastIndexOf('/') === args[1].length - 1) {
+    destPath = destPath + sourcePath.slice(lastBackslachIndex);
+  }
+
+  if (args[1].lastIndexOf('/') !== args[1].length - 1
+    && !destPath.includes('.')) {
+    const ext = sourcePath.slice(lastBackslachIndex).split('.')[1];
+
+    if (!fs.existsSync(destPath)) {
+      destPath = destPath + '.' + ext;
+    }
+
+    if (fs.existsSync(destPath)) {
+      destPath = destPath + fileName;
+    }
+  }
+
+  fs.rename(sourcePath, destPath, (err) => {
+    if (err) {
+      console.log(`
+        Error Found:,
+        ${err.message}
+      `);
+    } else {
+      console.log(`
+        Successfully moved ${fileName} to ${destPath}
+      `);
+    }
+  });
+};
+
+copy();
