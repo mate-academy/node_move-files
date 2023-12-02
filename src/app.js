998d83d8a14717c98b0ec1e819ec940bbc12e900
Dmytro <dmytro2105@gmail.com>
2023-12-02 17:45:34 +0200

    move files

--- src/app.js
+++ src/app.js
@@ -8,31 +8,55 @@ const path = require('path');
 const fileToCut = process.argv[2];
 const pathToPaste = process.argv[3];
 
-let checkIfDirExist = '';
-const pathToPasteArr = pathToPaste.split('/');
+const processPathToPaste = (pathToPasteW) => {
+  const pathToPasteArr = pathToPasteW.split('/');
+  let checkIfDirExist = '';
 
-if (pathToPasteArr[pathToPaste.length - 1] !== '/' && pathToPaste.includes('/')) {
-  if (pathToPaste.includes('/')) {
+  if (pathToPasteArr[pathToPasteArr.length - 1] !== '/' && pathToPasteW.includes('/')) {
     pathToPasteArr[pathToPasteArr.length - 1] = '';
-
     checkIfDirExist = pathToPasteArr.join('/');
   }
 
-  console.log(checkIfDirExist);
-} else if (fs.existsSync(`/${pathToPaste}`)) {
-  checkIfDirExist = `/${pathToPaste}/`;
-}
+  return checkIfDirExist;
+};
+
+const pathToPasteDir = processPathToPaste(pathToPaste);
+
+const relativePath = pathToPasteDir || pathToPaste;
+
+const dirExists = (dirPath) => {
+  try {
+    return fs.statSync(dirPath).isDirectory();
+  } catch (err) {
+    return false;
+  }
+};
 
 if (!fs.existsSync(fileToCut)) {
   console.error(`Error: Source file ${fileToCut} does not exist.`);
-} else if (!fs.existsSync(checkIfDirExist) && checkIfDirExist !== '') {
-  console.error(`Error: Destination directory ${checkIfDirExist} does not exist.`);
+} else if (!dirExists(relativePath)) {
+  const newFileName = path.basename(pathToPaste);
+  const newDestination = path.join(path.dirname(fileToCut), newFileName);
+
+  fs.rename(fileToCut, newDestination, (err) => {
+    if (err) {
+      console.error(`Error renaming file: ${err.message}`);
+    } else {
+      console.log('Rename complete!');
+    }
+  });
+} else if (dirExists(relativePath) && path.basename(pathToPaste) !== relativePath) {
+  const newDestination = path.join(relativePath, path.basename(pathToPaste));
+
+  fs.rename(fileToCut, newDestination, (err) => {
+    if (err) {
+      console.error(`Error renaming file: ${err.message}`);
+    } else {
+      console.log('Rename complete!');
+    }
+  });
 } else {
-  const newFileName = path.basename(pathToPaste); // Extract the file name
-
-  console.log(newFileName);
-
-  const newDestination = path.join(checkIfDirExist, newFileName);
+  const newDestination = path.join(pathToPaste, path.basename(fileToCut));
 
   fs.rename(fileToCut, newDestination, (err) => {
     if (err) {
