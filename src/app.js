8b19c8674823e8a1fcefa7a0056e47c93bf6608a
yurii-krymskiy <yurii.krymskyi@gmail.com>
2023-12-06 13:40:51 +0100

    new sol

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-// write code here
+'use strict';
+
+ /* es-lint-disable no-console */
+ const fs = require('fs').promises;
+ const path = require('path');
+
+ const moveFile = async() => {
+   const args = process.argv.slice(2);
+   const [source, destination] = args;
+
+   const sourceAbsolutePath = path.resolve(source);
+   const destinationAbsolutePath = path.resolve(destination);
+
+   try {
+     await fs.access(sourceAbsolutePath);
+
+     const isDirectory = destination.endsWith('/');
+
+     const finalDestination = isDirectory
+       ? path.join(destinationAbsolutePath, path.basename(sourceAbsolutePath))
+       : destinationAbsolutePath;
+
+     const destinationDirectory = path.dirname(finalDestination);
+
+     try {
+       await fs.access(destinationDirectory);
+     } catch (err) {
+       throw new Error(err);
+     }
+
+     fs.rename(sourceAbsolutePath, finalDestination);
+   } catch (err) {
+     throw new Error(err);
+   }
+ };
+
+ moveFile();
