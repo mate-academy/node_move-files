38b39f1f8b0008cf1439177c03c718ee9dc751bd
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-04 20:31:21 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -3,26 +3,42 @@
 const fs = require('fs');
 const path = require('path');
 
-// eslint-disable-next-line no-shadow
 const moveFile = (currentPath, newPath) => {
+  if (!currentPath || !fs.existsSync(currentPath)) {
+    throw new Error(`Invalid or non-existent source file: '${currentPath}'`);
+  }
+
   const fileData = fs.readFileSync(currentPath, 'utf-8');
+
+  if (!newPath) {
+    throw new Error('Destination path not provided.');
+  }
+
   const destinationPath = newPath.endsWith('/')
     ? newPath + path.basename(currentPath)
     : newPath;
 
-  if (newPath.includes('/') && !fs.existsSync(newPath + '/')) {
-    fs.writeFileSync(newPath, fileData);
-  } else {
-    fs.writeFileSync(destinationPath, fileData);
+  if (newPath.includes('/') && !fs.existsSync(newPath)) {
+    fs.mkdirSync(newPath, { recursive: true });
   }
 
-  fs.unlinkSync(currentPath);
+  try {
+    fs.writeFileSync(destinationPath, fileData);
+  } catch (err) {
+    throw new Error(`Error writing to destination file: ${err.message}`);
+  }
+
+  try {
+    fs.unlinkSync(currentPath);
+  } catch (err) {
+    throw new Error(`Error deleting source file: ${err.message}`);
+  }
 };
 
-const [currentPath, newPath] = process.argv.slice(2);
+try {
+  const [currentPath, newPath] = process.argv.slice(2);
 
-if (!fs.existsSync(newPath) && newPath.includes('/')) {
-  throw new Error(`this directory '${newPath}' does not exist.`);
+  moveFile(currentPath, newPath);
+} catch (error) {
+  throw new Error(`Error: ${error.message}`);
 }
-
-moveFile(currentPath, newPath);
