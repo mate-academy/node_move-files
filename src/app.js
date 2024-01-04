bc3952ba9c87fffd381638612936b71ebf46350b
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-04 19:18:20 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-// write code here
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+// eslint-disable-next-line no-shadow
+const moveFile = (currentPath, newPath) => {
+  const fileData = fs.readFileSync(currentPath, 'utf-8');
+  const destinationPath = newPath.endsWith('/')
+    ? newPath + path.basename(currentPath)
+    : newPath;
+
+  if (newPath.includes('/') && !fs.existsSync(newPath + '/')) {
+    fs.writeFileSync(newPath, fileData);
+  } else {
+    fs.writeFileSync(destinationPath, fileData);
+  }
+
+  fs.unlinkSync(currentPath);
+};
+
+const [currentPath, newPath] = process.argv.slice(2);
+
+if (!fs.existsSync(newPath) && newPath.includes('/')) {
+  throw new Error(`this directory '${newPath}' does not exist.`);
+}
+
+moveFile(currentPath, newPath);
