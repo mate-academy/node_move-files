81ad964fbebb86455ca632d4c3c6b73c04e716ec
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-05 12:55:45 +0200

    Added solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
-// write code here
+'use strict';
+
+/* eslint-disable no-console */
+
+const fs = require('fs');
+const path = require('path');
+
+const moveFile = (sourcePath, destinationPath) => {
+  const sourceAbsolutePath = path.resolve(sourcePath);
+  let destinationAbsolutePath = path.resolve(destinationPath);
+
+  if (destinationAbsolutePath.endsWith('/')) {
+    if (!fs.existsSync(destinationAbsolutePath)) {
+      throw new Error(
+        `Destination directory '${destinationAbsolutePath}' doesn't exist.`
+      );
+    }
+
+    destinationAbsolutePath = path.join(
+      destinationAbsolutePath,
+      path.basename(sourceAbsolutePath)
+    );
+  }
+
+  if (sourceAbsolutePath === destinationAbsolutePath) {
+    console.log('Source and destination paths are the same');
+
+    return;
+  }
+
+  try {
+    fs.renameSync(sourceAbsolutePath, destinationAbsolutePath);
+
+    console.log(
+      `File '${sourceAbsolutePath}' moved to '${destinationAbsolutePath}'`
+    );
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+moveFile(process.argv[2], process.argv[3]);
