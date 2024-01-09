df66601c8a32b40925fb3571a755b0adca010e09
Maksym Svoboda <maksim4ik71@gmail.com>
2024-01-10 00:41:22 +0200

    made solution for moving files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-// write code here
+/* eslint-disable no-console */
+'use strict';
+
+const { moveFiles } = require('./helper');
+const [source, destination] = process.argv.slice(2);
+
+if (!source) {
+  console.log('Please provide source and destination files');
+
+  return;
+}
+
+if (!destination) {
+  console.log('Please provide destination');
+
+  return;
+}
+
+if (destination[destination.length - 1] === '/') {
+  moveFiles(source, destination + source);
+} else {
+  moveFiles(source, destination);
+}
