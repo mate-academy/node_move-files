489b1b4344f4c0a63a88192202ba19f4f2947fb7
Mykola Maik <mykoluamaik@gmail.com>
2024-01-14 13:28:16 +0200

    Move files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-// write code here
+/* eslint-disable no-console */
+'use strict';
+
+const { moveFile } = require('./moveFile');
+
+const [source, destination] = process.argv.slice(2);
+
+if (!source || !destination) {
+  console.error('Usage: node index.js <source> <destination>');
+  process.exit(1);
+} else {
+  moveFile(source, destination);
+}
