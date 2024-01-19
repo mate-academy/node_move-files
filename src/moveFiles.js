705a80c3c5717e8bc374c89fe8654a7f1716e03e
Anastasiya145 <a.ivanova-external@powernext.com>
2024-01-19 11:41:06 +0100

    improvements

--- src/moveFiles.js
+++ src/moveFiles.js
@@ -1,35 +1,31 @@
 /* eslint-disable no-console */
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function normalizePaths(source, destination) {
   const currentPath = path.join(__dirname, source);
-  let newPath = '';
+  let newPath = "";
 
-  if (destination.endsWith('/')) {
+  if (destination.endsWith("/")) {
     const dirPath = path.join(__dirname, destination);
 
     newPath = path.join(dirPath, source);
-  } else {
-    if (destination.includes('./')) {
-      newPath = path.join(destination, source);
-    } else if (!destination.includes('/') && !destination.includes('.')) {
-      newPath = path.join(__dirname, destination, source);
+  } else if (
+    destination.includes("./") ||
+    (!destination.includes("/") && !destination.includes("."))
+  ) {
+    newPath = path.join(__dirname, destination, source);
 
-      // if directory is not exist, we rename the file
-      if (!fs.existsSync(path.dirname(newPath))) {
-        newPath = path.join(__dirname, './' + destination);
-      }
-    } else {
-      newPath = path.join(__dirname, destination);
+    if (!fs.existsSync(path.dirname(newPath))) {
+      newPath = path.join(__dirname, "./" + destination);
     }
+  } else {
+    newPath = path.join(__dirname, destination);
   }
 
-  return {
-    currentPath, newPath,
-  };
+  return { currentPath, newPath };
 }
 
 function moveFiles(source, destination) {
@@ -37,9 +33,9 @@ function moveFiles(source, destination) {
 
   fs.rename(currentPath, newPath, (err) => {
     if (err) {
-      console.log('Error Found:', err);
+      console.log("Error Found:", err);
     } else {
-      console.log('You succefully moved a file');
+      console.log("You succefully moved a file");
     }
   });
 }
