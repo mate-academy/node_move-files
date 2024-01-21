cef5e49327ec06f45aaacb3e382f3fd2a7e363b7
Kateryna Sydorenko <katerynnnna@gmail.com>
2024-01-21 17:46:36 +0200

    task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-// write code here
+'use strict';
+
+const fs = require('fs/promises');
+
+const app = async(file, to) => {
+  try {
+    await fs.rename(file, to);
+  } catch (error) {
+    throw error;
+  }
+};
+
+app(...process.argv.slice(2));
