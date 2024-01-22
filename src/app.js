08f0162c813d6c433050863124b1ac74e10b8d3f
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-22 20:54:39 +0100

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-// write code here
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const [, , fileName, folderName] = process.argv;
+
+const sourcePath = path.join('./src', fileName);
+const destinationPath = path.join('./src', folderName, fileName);
+
+if (!fs.existsSync(path.join('./src', folderName))) {
+  fs.mkdirSync(path.join('./src', folderName), { recursive: true });
+}
+
+fs.rename(sourcePath, destinationPath, (error) => {
+  if (error) {
+    // eslint-disable-next-line no-console
+    console.error(`Error: ${error.message}`);
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(`File '${fileName}' moved to '${folderName}' successfully.`);
+  }
+});
