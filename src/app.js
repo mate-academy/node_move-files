704efcf8e51e32f0edbcc04193d5538c5c17451c
Katsiaryna Prakofyeva <catherine.smiff@gmail.com>
2024-01-24 17:36:20 +0100

    fix: added solution, witch contain Pawel and Michal version :)))

--- src/app.js
+++ src/app.js
@@ -1,52 +1,63 @@
 /* eslint-disable no-console */
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-const move = async () => {
-  const [from, to] = process.argv.slice(-2);
+const move = async() => {
+  const [from, to] = process.argv.slice(2);
 
-  let newUrl = path.join(__dirname, '..', to);
+  let newUrl;
   const oldUrl = path.join(__dirname, from);
 
-  if (!path.extname(newUrl)) {
-    newUrl = path.join(__dirname, '..', to, from);
+  const checkPossibleDir = () => {
+    const checkIsDirect = path.join(__dirname, to, from);
+
+    const isDirectory = path.dirname(checkIsDirect);
+
+    return isDirectory;
+  };
+
+  const possibleDir = checkPossibleDir();
+
+  if (to.endsWith('/')) {
+    newUrl = path.join(__dirname, to, from);
+  } else {
+    try {
+      await fs.access(possibleDir);
+      newUrl = path.join(__dirname, to, from);
+    } catch (error) {
+      newUrl = path.join(__dirname, to);
+    }
   }
 
-  await checkDirectoryInPath(newUrl);
+  const sourseDirectory = path.dirname(oldUrl);
+  const destDirectory = path.dirname(newUrl);
 
-  fs.stat(oldUrl, (error, stats) => {
-    if (error) {
-      throw new Error('Nie istnuje');
+  if (sourseDirectory === destDirectory) {
+    try {
+      await fs.rename(oldUrl, newUrl);
+      console.log('Successfully renamed');
+
+      return;
+    } catch (error) {
+      console.log('Error in renamed process');
     }
+  };
 
-    if (stats.isDirectory()) {
-      console.log('Is directory');
-    }
-  });
-
-  fs.rename(oldUrl, newUrl, (error) => {
-    if (error) {
-      throw new Error('Not rename');
-    }
-
-    console.log('Name renamed');
-  });
+  try {
+    await fs.access(destDirectory);
+    // Here is 2 working method:
+    // first with rename,
+    await fs.rename(oldUrl, newUrl);
+    console.log('Successfully moved');
+    // second with create new and delete old file
+    // const fileData = await fs.readFile(oldUrl, 'utf-8');
+    // await fs.writeFile(newUrl, fileData);
+    // await fs.unlink(oldUrl);
+  } catch (err) {
+    console.error('Destination directory does not exist');
+  }
 };
 
 move();
-
-async function checkDirectoryInPath(filePath) {
-  const dirname = path.dirname(filePath);
-
-  try {
-    await fs.promises.access(dirname, fs.constants.F_OK);
-  } catch (error) {
-    fs.mkdir(dirname, (err) => {
-      if (err) {
-        throw new Error('Can\'t make directory');
-      }
-    });
-  }
-}
