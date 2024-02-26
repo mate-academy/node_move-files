27412a969ab08823cd96e1b7c0b4712517bb71ec
Dmytro Levchenko <levchenkod91@gmail.com>
2024-02-26 21:52:29 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-// write code here
+/* eslint-disable no-console */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const [from, to] = process.argv.slice(2);
+
+async function moveFiles(source, dest) {
+  const fullPathSource = path.resolve(source);
+  let fullPathDest = path.resolve(dest);
+  const fileName = path.basename(fullPathSource);
+
+  if (fs.existsSync(fullPathDest) && fs.statSync(fullPathDest).isDirectory()) {
+    fullPathDest = path.join(fullPathDest, fileName);
+  }
+
+  try {
+    fs.renameSync(fullPathSource, fullPathDest);
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+moveFiles(from, to);
