bcce7531b5b96f861c9005bc91827d19f3030dbb
Dmytro Levchenko <levchenkod91@gmail.com>
2024-02-27 10:27:29 +0200

    improve the code

--- src/app.js
+++ src/app.js
@@ -1,25 +1,34 @@
 /* eslint-disable no-console */
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
 const [from, to] = process.argv.slice(2);
 
 async function moveFiles(source, dest) {
+  if (!source || !dest) {
+    throw new Error('no params');
+  }
+
+  if (source === dest) {
+    return;
+  }
+
   const fullPathSource = path.resolve(source);
   let fullPathDest = path.resolve(dest);
   const fileName = path.basename(fullPathSource);
 
-  if (fs.existsSync(fullPathDest) && fs.statSync(fullPathDest).isDirectory()) {
+  if (fs.existsSync(fullPathDest)) {
     fullPathDest = path.join(fullPathDest, fileName);
   }
 
   try {
-    fs.renameSync(fullPathSource, fullPathDest);
+    await fs.rename(fullPathSource, fullPathDest);
+    console.log('Success!');
   } catch (e) {
     throw new Error(e);
-  }
+  };
 };
 
 moveFiles(from, to);
