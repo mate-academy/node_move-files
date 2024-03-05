a226ffdbde75c61f658994f75f85bfa44607dd20
Artem <artemmalikov742@gmail.com>
2024-03-05 11:03:19 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-// write code here
+/* eslint-disable prefer-const */
+/* eslint-disable no-console */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const [sourcePath, destinationPath] = process.argv.slice(2);
+
+try {
+  if (!sourcePath || !destinationPath) {
+    throw new Error('Only one argument is provided');
+  }
+
+  let destinationAbsolutePath = path.resolve(destinationPath);
+
+  if (fs.existsSync(destinationAbsolutePath)
+    && fs.statSync(destinationAbsolutePath).isDirectory()) {
+    destinationAbsolutePath
+      = path.join(destinationAbsolutePath, path.basename(sourcePath));
+  }
+
+  fs.renameSync(sourcePath, destinationAbsolutePath);
+} catch (error) {
+  console.error(error);
+}
