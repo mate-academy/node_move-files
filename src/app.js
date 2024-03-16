65f9dbbbf920aa1089ca965852644fe02ec99afe
boytrend0108 <boytrend@gmail.com>
2024-03-16 12:23:32 +0200

    complete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+
+const [source, destination] = process.argv.slice(2);
+
+try {
+  if (!source || !destination) {
+    throw new Error('Wrong params');
+  }
+
+  let destinationAbsPath = path.resolve(destination);
+
+  if (
+    fs.existsSync(destinationAbsPath) &&
+    fs.statSync(destinationAbsPath).isDirectory()
+  ) {
+    destinationAbsPath = path.join(destinationAbsPath, path.basename(source));
+  }
+
+  fs.renameSync(source, destinationAbsPath);
+} catch (err) {
+  console.error(err);
+}
