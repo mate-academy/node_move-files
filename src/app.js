8119605a7a8fe8f2e35e202e8eb44e6017151b89
Basil-Panasiuk <v.panasiuk.job@gmil.com>
2024-04-15 22:34:55 +0300

    Feat: implement moving files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs/promises');
+const path = require('path');
+
+const [source, destination] = process.argv.slice(2);
+
+const isDirectory = async (dirPath) => {
+  try {
+    const stats = await fs.stat(dirPath);
+
+    return stats.isDirectory();
+  } catch (_) {
+    return false;
+  }
+};
+
+const moveFile = async () => {
+  try {
+    let destPath = destination;
+
+    if (await isDirectory(destPath)) {
+      destPath = path.join(destPath, path.basename(source));
+    }
+    await fs.rename(source, destPath);
+  } catch (_) {
+    console.error('Failed to move file');
+  }
+};
+
+moveFile();
