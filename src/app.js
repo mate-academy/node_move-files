25e1185f4c42c22e2502318d5eab4980506ae9b5
IShamkii <fanmetalheavy@gmail.com>
2025-02-15 12:19:45 +0200

    task solution with promises

--- src/app.js
+++ src/app.js
@@ -7,14 +7,14 @@ const path = require('path');
 async function moveFile() {
   const args = process.argv.slice(2);
 
-  const pathFromMove = args[0];
-  let pathToMove = args[1];
-
   if (args.length < 2) {
     console.error('You did not pass all parameters.');
 
     return;
   }
+
+  const pathFromMove = args[0];
+  let pathToMove = args[1];
 
   if (!fs.existsSync(path.dirname(pathToMove))) {
     console.error('File or directory does not exist');
@@ -28,15 +28,25 @@ async function moveFile() {
     return;
   }
 
-  const stat = fs.existsSync(pathToMove) ? fs.statSync(pathToMove) : null;
+  // const stat = fs.existsSync(pathToMove) ? fs.statSync(pathToMove) : null;
+  const stat = await fs.promises.stat(pathToMove).catch(() => null);
 
   if (stat && stat.isDirectory()) {
     pathToMove = path.join(pathToMove, path.basename(pathFromMove));
   }
 
+  // fs.promises
+  //   .cp(pathFromMove, pathToMove)
+  //   .then(() => fs.promises.unlink(pathFromMove))
+  //   .catch((err) => {
+  //     console.error(
+  // eslint-disable-next-line max-len
+  //       `Error occurred while moving the file: ${err.code}\npathFromMove - ${pathFromMove}\npathToMove - ${pathToMove}`,
+  //     );
+  //   });
   try {
-    fs.cpSync(pathFromMove, pathToMove);
-    fs.rmSync(pathFromMove);
+    await fs.promises.copyFile(pathFromMove, pathToMove);
+    await fs.promises.unlink(pathFromMove);
 
     console.log(
       `File successfully moved from ${pathFromMove} to ${pathToMove}`,
