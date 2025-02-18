40a916e5945b4cc747ad6c090d8a510b31ac382e
Svitlana <svitlana.gaiduk@gmail.com>
2025-02-18 15:55:54 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+
+const source = process.argv[2];
+const destination = process.argv[3];
+
+try {
+  if (!source || !destination) {
+    throw new Error('Source and destination files must be provided.');
+  }
+
+  if (!fs.existsSync(source)) {
+    throw new Error(`Source file does not exist.`);
+  }
+
+  if (
+    destination.endsWith('/') ||
+    (fs.existsSync(destination) && fs.statSync(destination).isDirectory())
+  ) {
+    const finalDestination = path.join(destination, path.basename(source));
+
+    fs.renameSync(source, finalDestination);
+  } else {
+    fs.renameSync(source, destination);
+  }
+} catch (error) {
+  console.error(error);
+}
