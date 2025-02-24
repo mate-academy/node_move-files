eada5f806974f30927836f4c5d2f70d40f19d1a6
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2025-02-24 19:58:43 +0200

    Add new file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('node:fs');
+const path = require('node:path');
+
+const main = function () {
+  const [pathIn, pathOut] = process.argv.slice(2);
+
+  if (!pathIn || !pathOut) {
+    console.error('It must have two passed args.');
+  }
+
+  if (!fs.existsSync(pathIn)) {
+    console.error('The file does not exists.');
+  }
+
+  try {
+    let destinationPath = pathOut;
+
+    if (fs.existsSync(pathOut) && fs.statSync(pathOut).isDirectory()) {
+      destinationPath = path.join(pathOut, path.basename(pathIn));
+    }
+    fs.renameSync(pathIn, destinationPath);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
+main();
