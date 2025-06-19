690f0cd825963a3094843a36ffbda4e4e4706fec
Voloshynd <Voloshynd13@gmail.com>
2025-06-19 20:05:06 +0200

    Solution of task node_move-files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,45 @@
-// write code here
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+function moveFile() {
+  const [source, destination] = process.argv.slice(2);
+
+  if (process.argv.slice(2).length !== 2) {
+    throw new Error('Provide both source and destination paths');
+  }
+
+  const sourcePath = path.resolve(source);
+  const destinationPath = path.resolve(destination);
+
+  if (!fs.existsSync(sourcePath) || !fs.statSync(sourcePath).isFile()) {
+    throw new Error('Source file does not exist or is not a file');
+  }
+
+  const destinationLooksLikeDir =
+    destination.endsWith('/') ||
+    (fs.existsSync(destinationPath) &&
+      fs.statSync(destinationPath).isDirectory());
+
+  if (destinationLooksLikeDir) {
+    if (!fs.existsSync(destinationPath)) {
+      throw new Error('Destination directory does not exist');
+    }
+
+    const newFilePath = path.join(destinationPath, path.basename(sourcePath));
+
+    fs.renameSync(sourcePath, newFilePath);
+  } else {
+    const destinationDir = path.dirname(destinationPath);
+
+    if (!fs.existsSync(destinationDir)) {
+      throw new Error(' Destination directory does not exist');
+    }
+    fs.renameSync(sourcePath, destinationPath);
+  }
+
+  return `File moved successfully: ${sourcePath} -> ${destinationPath}`;
+}
+
+moveFile();
