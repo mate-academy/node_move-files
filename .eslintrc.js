687b3488581fecbb9a6ca2b90e4c3da7b846f1ed
Anton Kuchmasov <antek.ukraine@gmail.com>
2025-08-10 16:27:16 +0300

    solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-  extends: '@mate-academy/eslint-config',
+  extends: "@mate-academy/eslint-config",
   env: {
-    jest: true
+    jest: true,
   },
   rules: {
-    'no-proto': 0
+    "no-proto": 0,
+    strict: 0,
+    "no-console": 0
   },
-  plugins: ['jest']
+  plugins: ["jest"],
 };
--- src/app.js
+++ src/app.js
@@ -1,1 +1,87 @@
-// write code here
+const fs = require('fs/promises');
+const path = require('path');
+
+async function moveFiles() {
+  const args = process.argv.slice(2);
+
+  if (args.length !== 2) {
+    console.error('Invalid input!');
+
+    return;
+  }
+
+  const [source, destination] = args;
+
+  try {
+    await fs.access(source);
+  } catch (err) {
+    console.error('Source file does not exist!');
+
+    return;
+  }
+
+  // if source & dest is in one folder, we can just rename them
+  if (path.dirname(source) === path.dirname(destination)) {
+    try {
+      await fs.rename(source, destination);
+    } catch (err) {
+      console.error('Error renaming file:', err.message);
+    }
+
+    return;
+  }
+
+  // if destination ends with ('/'), this dir should exists
+  if (destination.endsWith(path.sep)) {
+    try {
+      const stats = await fs.stat(destination);
+
+      if (!stats.isDirectory()) {
+        console.error('Destination is not a directory!');
+
+        return;
+      }
+    } catch (err) {
+      console.error('Destination directory does not exist!');
+
+      return;
+    }
+
+    const fullDestPath = path.join(destination, path.basename(source));
+
+    try {
+      await fs.rename(source, fullDestPath);
+    } catch (err) {
+      console.error('Error moving file:', err.message);
+    }
+
+    return;
+  }
+
+  // if destination exists, and it's a dir
+  try {
+    const stats = await fs.stat(destination);
+
+    if (stats.isDirectory()) {
+      const fullDestPath = path.join(destination, path.basename(source));
+
+      try {
+        await fs.rename(source, fullDestPath);
+      } catch (err) {
+        console.error('Error moving file:', err.message);
+      }
+
+      return;
+    }
+  } catch (err) {
+  }
+
+  // just renaming source to destination files in other cases
+  try {
+    await fs.rename(source, destination);
+  } catch (err) {
+    console.error('Error renaming/moving file:', err.message);
+  }
+}
+
+moveFiles();
