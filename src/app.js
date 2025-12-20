b3bf1cccc59984d9d30286d0cf6cb7975844ad91
Vouchyk <user@users-MacBook-Air.local>
2025-12-20 23:40:50 +0000

    Tasks solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+const [oldPath, newPathInput] = process.argv.slice(2);
+
+function app() {
+  if (!oldPath || !newPathInput) {
+    console.error('Please provide both old path and new path');
+
+    return;
+  }
+
+  let destination = newPathInput;
+
+  const stat = fs.existsSync(newPathInput) && fs.statSync(newPathInput);
+
+  if ((stat && stat.isDirectory()) || newPathInput.endsWith('/')) {
+    destination = path.join(newPathInput, path.basename(oldPath));
+  }
+
+  if (oldPath === destination) {
+    return;
+  }
+
+  try {
+    fs.renameSync(oldPath, destination);
+    console.log(`${oldPath} was moved to ${destination}`);
+  } catch (err) {
+    console.error(`The file could not be moved. Error: ${err.message}`);
+  }
+}
+
+app();
+
+module.exports = app;
