d3107d70ca3cd9ae5b7b220be4582a1f42fb2f5e
Nazarii-Lesniak <naziksay@gmail.com>
2026-03-18 18:13:42 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,43 @@
-// write code here
+/* eslint-disable no-console */
+'use strict';
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const [sourcePath, destPath] = process.argv.slice(2);
+
+async function move() {
+  if (!sourcePath || !destPath) {
+    console.error('Please specify the source and destination file paths');
+
+    return;
+  }
+
+  const fileName = path.basename(sourcePath);
+
+  let finalPath;
+
+  if (destPath.endsWith('/') || destPath.endsWith('\\')) {
+    finalPath = path.join(destPath, fileName);
+  } else {
+    try {
+      const stats = await fs.stat(destPath);
+
+      if (stats.isDirectory()) {
+        finalPath = path.join(destPath, fileName);
+      } else {
+        finalPath = destPath;
+      }
+    } catch (error) {
+      finalPath = destPath;
+    }
+  }
+
+  try {
+    await fs.rename(sourcePath, finalPath);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+move();
