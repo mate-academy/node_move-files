2cec740c4a62c1526a261a3672bbd293a2fb25a6
Vladislav-Korsun <vladosik385@gmail.com~>
2026-03-26 20:28:00 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
-// write code here
+/* eslint-disable no-console */
+
+const fs = require('fs');
+const path = require('path');
+
+function moveFile() {
+  const arg = process.argv.slice(2);
+
+  const source = arg[0];
+  const destination = arg[1];
+
+  if (!source || !destination) {
+    console.error('Invalid arguments');
+
+    return;
+  }
+
+  if (source === destination) {
+    return;
+  }
+
+  let finalDestination = destination;
+
+  if (
+    destination.endsWith('/') ||
+    (fs.existsSync(destination) && fs.statSync(destination).isDirectory())
+  ) {
+    finalDestination = path.join(destination, path.basename(source));
+  }
+
+  try {
+    fs.renameSync(source, finalDestination);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+moveFile();
