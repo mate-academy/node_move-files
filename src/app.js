ad52ae6056a0ab12abd9c2294fbf705b3489c9d5
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-04-12 06:35:24 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ function main() {
   const fs = require('fs');
   const path = require('path');
 
+  if (process.argv.slice(2).length !== 2) {
+    console.error('Usage: node app.js <file> <newFile>');
+
+    return;
+  }
+
   const [file, newFile] = process.argv.slice(2);
 
   if (!fs.existsSync(file)) {
@@ -14,6 +20,7 @@ function main() {
   const destExists = fs.existsSync(newFile);
   const stats = destExists ? fs.statSync(newFile) : null;
   const fileName = path.basename(file);
+  const parentDir = path.dirname(newFile);
 
   if (newFile.endsWith('/')) {
     if (!destExists) {
@@ -22,13 +29,28 @@ function main() {
       return;
     }
 
-    fs.renameSync(file, `${newFile}${fileName}`);
+    const newPath = path.join(newFile, fileName);
+
+    fs.renameSync(file, newPath);
 
     return;
   }
 
-  if (stats.isDirectory()) {
-    fs.renameSync(file, `${newFile}${fileName}`);
+  if (stats && stats.isDirectory()) {
+    const newPath = path.join(newFile, fileName);
+
+    if (!fs.existsSync(parentDir)) {
+      console.error(`${newPath} doesn't exist`);
+
+      return;
+    }
+    fs.renameSync(file, newPath);
+
+    return;
+  }
+
+  if (!fs.existsSync(parentDir)) {
+    console.error(`Invalid destination path: ${newFile}`);
 
     return;
   }
