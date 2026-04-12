aaede618187361a6a5e57c9d6a68885ee9dd1e0e
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-04-12 06:01:28 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
-// write code here
+/* eslint-disable no-console */
+function main() {
+  const fs = require('fs');
+  const path = require('path');
+
+  const [file, newFile] = process.argv.slice(2);
+
+  if (!fs.existsSync(file)) {
+    console.error(`${file} doesn't exist`);
+
+    return;
+  }
+
+  const destExists = fs.existsSync(newFile);
+  const stats = destExists ? fs.statSync(newFile) : null;
+  const fileName = path.basename(file);
+
+  if (newFile.endsWith('/')) {
+    if (!destExists) {
+      console.error(`${newFile} doesn't exist`);
+
+      return;
+    }
+
+    fs.renameSync(file, `${newFile}${fileName}`);
+
+    return;
+  }
+
+  if (stats.isDirectory()) {
+    fs.renameSync(file, `${newFile}${fileName}`);
+
+    return;
+  }
+
+  fs.renameSync(file, newFile);
+}
+
+main();
