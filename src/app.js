29a1954261b1c2b14976cae4b0c4594784eddacc
Kirill <kkoctuk2005@gmail.com>
2026-07-10 20:58:22 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,64 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+
+function move() {
+  const [source, destination] = process.argv.slice(2);
+
+  if (!source || !destination) {
+    console.error('Both paths should be provided');
+
+    return;
+  }
+
+  if (source === destination) {
+    return;
+  }
+
+  if (!fs.existsSync(source)) {
+    console.error('Non-existent source file');
+
+    return;
+  }
+
+  if (destination.endsWith('/')) {
+    if (!fs.existsSync(destination)) {
+      console.error("Directory doesn't exist");
+
+      return;
+    }
+
+    const fileName = path.basename(source);
+    const finalDest = path.join(destination, fileName);
+
+    fs.rename(source, finalDest, (renameError) => {
+      if (renameError) {
+        console.error('Move error');
+      }
+    });
+
+    return;
+  }
+
+  fs.stat(destination, (destError, destStats) => {
+    let finalDest = destination;
+
+    if (!destError && destStats.isDirectory()) {
+      const fileName = path.basename(source);
+
+      finalDest = path.join(destination, fileName);
+    } else if (destError && destError.code !== 'ENOENT') {
+      console.error('Move error');
+
+      return;
+    }
+
+    fs.rename(source, finalDest, (renameError) => {
+      if (renameError) {
+        console.error('Move error');
+      }
+    });
+  });
+}
+
+move();
